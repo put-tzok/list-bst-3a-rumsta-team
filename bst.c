db7a402e01de1126b115d1fb5160e5073c173f42
rrumplewicz <62466597+rrumplewicz@users.noreply.github.com>
2020-04-18 00:38:50 +0200

    Update bst.c

--- bst.c
+++ bst.c
@@ -5,7 +5,7 @@
 #include <signal.h>
 #include <time.h>
 
-unsigned int ns[] = { 10, /* TODO: fill values which will be used as lists' sizes */ };
+unsigned int ns[] = { 10, 100, 1000, 10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000};
 
 // each tree node contains an integer key and pointers to left and right children nodes
 struct node {
